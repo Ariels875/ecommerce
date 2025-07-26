8f70fabd41be2d4ed2db73b755935a8908b75375
Ariels <ascastro875@gmail.com>
2025-07-25 22:19:43 -0500

    Updated something...

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -170,7 +170,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
         <div className="mt-8 border-t border-gray-700 pt-8 text-center">
-          <p className="text-gray-400">&copy; 2024 Ariels Store. Todos los derechos reservados.</p>
+          <p className="text-gray-400">&copy; 2025 Ariels Store. Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
